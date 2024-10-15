63c413a6c15f9e0f28ec028250f863d1d05a6d71
Faarax Cabdulaahi <faarax2024@gmail.com>
2024-10-16 02:18:30 +0300

    Day 23 of js problems code has been added to the repo.

---
+++ Day-23/script.js
@@ -0,0 +1,8 @@
+const textElement = document.querySelector("#text");
+textElement.textContent = `Hello World`;
+textElement.style.fontSize = `${30}px`;
+textElement.style.backgroundColor = `orange`;
+textElement.style.color = `blue`;
+textElement.style.padding = `${16}px`;
+textElement.style.fontFamily = `sans-serif`;
+textElement.style.fontWeight = `bold`;
